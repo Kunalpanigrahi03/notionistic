8a4c59b1761ea88326691795d09be2960a44b993
Kunal <Kunal@codewithkunal.com>
2024-04-30 12:13:49 +0530

    Updated gemini.ts to generate content from more yt channels Updated notion.ts for formatting and visuals

--- src/notion.js
+++ src/notion.js
@@ -13,14 +13,20 @@ exports.createNotionPage = void 0;
 const client_1 = require("@notionhq/client");
 const notion_client = new client_1.Client({ auth: process.env.NOTION_ACCESS_TOKEN });
 const pageID = process.env.NOTION_PAGE_ID;
-function createNotionPage(message, description) {
+function createNotionPage(_message_, _description_) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const response = yield notion_client.pages.create({
                 "parent": { "page_id": pageID },
+                "cover": {
+                    "type": "external",
+                    "external": {
+                        "url": "https://media.licdn.com/dms/image/D4D12AQF6mW4EuB-99Q/article-cover_image-shrink_720_1280/0/1692951785182?e=2147483647&v=beta&t=I6_1-aBTAg0fihJHret-C4hRNuffBu8JyrqKfXsm74w"
+                    }
+                },
                 "properties": {
                     "title": {
-                        "title": [{ "type": "text", "text": { "content": message } }]
+                        "title": [{ "type": "text", "text": { "content": _message_ } }]
                     }
                 },
                 "children": [
@@ -28,7 +34,22 @@ function createNotionPage(message, description) {
                         "object": "block",
                         "type": "paragraph",
                         "paragraph": {
-                            "rich_text": [{ "type": "text", "text": { "content": description } }]
+                            "rich_text": [
+                                {
+                                    "type": "text",
+                                    "text": {
+                                        "content": _description_
+                                    },
+                                    "annotations": {
+                                        "bold": false,
+                                        "italic": true,
+                                        "strikethrough": false,
+                                        "underline": false,
+                                        "code": false,
+                                        "color": "orange",
+                                    }
+                                }
+                            ]
                         }
                     }
                 ]
--- src/notion.ts
+++ src/notion.ts
@@ -1,29 +1,51 @@
 import { Client } from '@notionhq/client';
 
 const notion_client = new Client({ auth: process.env.NOTION_ACCESS_TOKEN });
+
 const pageID = process.env.NOTION_PAGE_ID;
 
-  export async function createNotionPage(message:string, description:string) {
-    try {
-      const response = await notion_client.pages.create({
-        "parent": { "page_id": pageID! },
-        "properties": {
-            "title": {
-          "title": [{ "type": "text", "text": { "content": message } }]
-            }
-        },
-        "children": [
+export async function createNotionPage(_message_: string, _description_: string) {
+  try {
+    const response = await notion_client.pages.create({
+      "parent": { "page_id": pageID! },
+      "cover": {
+        "type": "external",
+        "external": {
+          "url": "https://media.licdn.com/dms/image/D4D12AQF6mW4EuB-99Q/article-cover_image-shrink_720_1280/0/1692951785182?e=2147483647&v=beta&t=I6_1-aBTAg0fihJHret-C4hRNuffBu8JyrqKfXsm74w"
+        }
+      },
+      "properties": {
+        "title": {
+          "title": [{ "type": "text", "text": { "content": _message_ } }]
+        }
+      },
+      "children": [
         {
           "object": "block",
           "type": "paragraph",
           "paragraph": {
-            "rich_text": [{ "type": "text", "text": { "content": description } }]
+            "rich_text": [
+              {
+                "type": "text",
+                "text": {
+                  "content": _description_
+                },
+                "annotations": {
+                  "bold": false,
+                  "italic": true,
+                  "strikethrough": false,
+                  "underline": false,
+                  "code": false,
+                  "color": "orange",
+                }
+              }
+            ]
           }
         }
       ]
-      });
-      console.log("Page created successfully:");
-    } catch (error) {
-      console.error("Error creating page:",error);
-    }  
+    });
+    console.log("Page created successfully:");
+  } catch (error) {
+    console.error("Error creating page:", error);
   }
+}
